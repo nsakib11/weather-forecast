c211969a2c852d7a9e0a4e87436308df54b2dcfb
todos <nazmussakib409@gmail.com>
2024-01-15 22:14:48 +0600

    Add sky condition

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ days = st.slider("forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    # Get the temperature/sky data
+    filtered_data = get_data(place, days)
 
-def get_data(days):
-    dates = []
-    temperatures =[]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
 
-d, t = get_data(days)
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C"})
-st.plotly_chart(figure)
+    if option == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
+        sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_path = [images[condition] for condition in sky_condition]
+        print(sky_condition)
+        st.image(image_path, width=115)
